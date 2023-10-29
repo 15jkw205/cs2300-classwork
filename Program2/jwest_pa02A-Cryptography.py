98f4a2f217cd64815083976c58dfc2ae95884e65
15jkw205 <brawlgodbs@gmail.com>
2023-10-29 01:29:18 -0600

    Program2 Part A updated

--- Program2/jwest_pa02A-Cryptography.py
+++ Program2/jwest_pa02A-Cryptography.py
@@ -5,7 +5,73 @@
 
 import numpy as np
 
-# Part A1.1: Set up the 4x4 coding matrix
+
+# FUNCTIONS!!! 
+
+# Converts the string of text into its hexadecimal-unicode equivalent
+def text_to_unicode_hex(text):
+    unicode_hex = [hex(ord(char))[2:] for char in text]
+    return unicode_hex
+
+
+# Converts a list of unicode-hexadecimal into its a numpy array equivalent with
+# 4 columns of decimal numbers 
+def unicode_hex_to_decimal(input_list):
+    
+    # Convert Unicode-hex numbers to decimal values
+    decimal_values = [int(hex_str, 16) for hex_str in input_list]
+
+    # Determine the number of rows needed to have 4 columns
+    num_columns = 4
+    num_rows = (len(decimal_values) + num_columns - 1) // num_columns
+
+    # Calculate the total number of elements needed to fill the matrix
+    total_elements = num_rows * num_columns
+
+    # Fill in the remaining slots with zeros
+    while len(decimal_values) < total_elements:
+        decimal_values.append(0)
+
+    # Reshape the list into a numpy matrix with 4 columns
+    matrix = np.array(decimal_values).reshape(num_rows, num_columns)
+
+    return matrix
+
+
+# Converts a numpy matrix back into a string of text
+def numpy_matrix_to_text(matrix):
+    num_rows, num_columns = matrix.shape  # Get the dimensions of the matrix
+    text_values = []
+
+    for col in range(num_columns):
+        for row in range(num_rows):
+            value = matrix[row, col] # Convert each element to an integer
+
+            if (value == 51):
+                value += 1
+                
+            if (value != 0 & value != 1): 
+                text_values.append(str(value))
+
+    # Join the list of values into a single string with spaces
+    text_string = ' '.join(text_values)
+    
+    return text_string
+
+
+# Converts ASCII values back to characters
+def ascii_to_characters(input_str):
+    # Split the input string into a list of ASCII values
+    ascii_values = input_str.split()
+
+    # Convert each ASCII value to a character and join them
+    characters = ''.join([chr(int(ascii_value)) for ascii_value in ascii_values])
+
+    return characters
+
+
+
+# Part A1.1: The invertible endcoding matrix - Matrix A
 
 A = np.array ([
     [1, -1, -1, 1],
@@ -15,72 +81,96 @@ A = np.array ([
 ])
 
 
+print("Welcome to Part A of Programming Assignment #2 in CS 2300!\n", end='\n')
+print("\nHere are the answers to Part A2.1\n", end='\n')
+
+# Open the file for reading
+with open("input-A21.txt", "r") as file:
+    password = file.read()
+
+
+# Part A1.2: Encrypting the message - Matrix B
+unicode_hex_list = text_to_unicode_hex(password)
+encrypted_matrix = unicode_hex_to_decimal(unicode_hex_list)
+
+B_partA_21 = np.transpose(encrypted_matrix)
+
+print("This is Matrix: B (Encrypting the message) --> ")
+print(B_partA_21)
+
+'''
+Question #1: Why does this plaintext matrix have to be made up of column vectors of 4
+elements?
+*** Answer: In order to multiply matrices, the column size of the first matrix must
+be the same as the row size of the second matrix, i.e, Matrix A has 4 rows and 4 columns
+which means that Matrix B must have 4 rows, so the matrices can multiply properly. This
+is a fundamental rule of multiplying matrices!!!
+
+1st Matrix ---> (nx4) * (4xm) <-- 2nd Matrix --- where n and m are all real numbers
+
+'''
+
+
+# encode the message - Cipher Matrix C
+C_partA_21 = np.dot(A, B_partA_21) # Matrix A * Matrix B
+print("\nThis is Matrix: C (Encode the message) --> ")
+print(C_partA_21)
+print()
+
+
+
+print("\nHere are the answers to Part A2.2\n", end='\n')
+
+# Part A1.4 The inverse of Matrix A - Used for decrypting the message
 A_inv = np.linalg.inv(A)
 
-# converts the string of text into its hexadecimal-unicode equivalent
-def text_to_unicode_hex(text):
-    unicode_hex = [hex(ord(char))[2:] for char in text]
-    return unicode_hex
+# Read the numbers from the input file
+with open("input-A22.txt", "r") as file:
+    numbers = [int(num) for num in file.read().split()]
+
+# Determine the number of rows needed to have 4 columns
+num_columns = 4
+num_rows = (len(numbers) + num_columns - 1) // num_columns
+
+# Calculate the total number of elements needed to fill the matrix
+total_elements = num_rows * num_columns
+
+# Fill in the remaining slots with zeros
+while len(numbers) < total_elements:
+    numbers.append(0)
+
+# Reshape the list into a numpy matrix with 4 columns
+matrix = np.array(numbers).reshape(num_rows, num_columns)
 
 
-# Example usage:
-text = "NCS-2014"
-unicode_hex_list = text_to_unicode_hex(text)
-print(unicode_hex_list)
+# Encrypting the matrix - Matrix B
+B_partA_22 = np.transpose(matrix)
+
+# Print the resulting matrix
+print("This is Matrix: B (Part A2.2) --> ")
+print(B_partA_22)
 
 
-# Part A1.2: Function to convert decimal value to character
-def decimal_to_char(decimal):
-    return chr(decimal)
-
-# Part A1.2: Function to encode a plaintext message
-def encode_message(message):
-    encoded_message = []
-    for char in message:
-        decimal_value = char_to_decimal(char)
-        encoded_vector = [decimal_value, 0, 0, 0]
-
-        # Split the vector into 4-element column vectors
-        while len(encoded_vector) > 0:
-            encoded_message.append(encoded_vector[:4])
-            encoded_vector = encoded_vector[4:]
-
-    return encoded_message
-
-# Part A1.3: Function to encode the message using matrix A
-def cipher_matrix(A, B):
-    encoded_matrix = []
-    for i in range(len(B)):
-        encoded_row = []
-        for j in range(len(A[0])):
-            sum = 0
-            for k in range(len(A)):
-                sum += A[i][k] * B[k][j]
-            encoded_row.append(sum)
-        encoded_matrix.append(encoded_row)
-
-    return encoded_matrix
-
-    return decoded_message
-
-# Part A2.1: Accept user input for a plaintext message
-plaintext = input("Enter a plaintext message: ")
-
-# Part A2.1: Encode the message using matrix A
-encoded_message = encode_message(plaintext)
-
-# Part A2.2: Decode the message using the inverse of matrix A
-A_inv = [
-    [2, -3, -1, 3],
-    [1, -3, -1, 3],
-    [1, -5, -2, -1],
-    [1, 4, 2, 2]
-]
-
-decoded_message = decode_message(encoded_message, A_inv)
-
-# Part A2.2: Display the results
-print("Encoded Message:", encoded_message)
-print("Decoded Message:", "".join([decimal_to_char(decimal) for decimal in decoded_message]))
+# Create a cipher Matrix from the file matrix
+C_partA_22 = np.dot(A, B_partA_22) # Matrix A * Matrix B
+print("\nThis is Matrix: C (Encode the message) --> ")
+print(C_partA_22)
 
 
+# Decoding/Decrypting the message
+resulting_matrix = np.dot(A_inv, C_partA_22)
+resulting_matrix = resulting_matrix.astype(int)
+
+string_numbers = numpy_matrix_to_text(resulting_matrix)
+
+final_string = ascii_to_characters(string_numbers)
+
+# Export the result to a new text file
+with open("output-A22.txt", "w") as file:
+    file.write(final_string)
+
+print("\n" + final_string, end='\n')
+print("Result has been saved to 'output-A22.txt'")
+
+
+# End of Part A of Programming Assignment #2 - CS2300
